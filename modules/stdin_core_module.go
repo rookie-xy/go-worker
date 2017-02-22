8e45ae51ae82337ddbe23ffcb63aebc51e43afd7
mengshi <meng1985626@163.com>
2017-02-22 16:18:23 +0800

    update

--- modules/stdin_core_module.go
+++ modules/stdin_core_module.go
@@ -5,108 +5,94 @@
 package modules
 
 import (
-	"unsafe"
-	"fmt"
-	. "worker/types"
+      "unsafe"
+      "fmt"
+    . "worker/types"
 )
 
 type AbstractStdinCore struct {
-	*AbstractCycle
-	*AbstractFile
+    *AbstractCycle
+    *AbstractFile
 
-	status   bool
-	channal  string
+     status   bool
+     channal  string
 }
 
 func NewStdinCore() *AbstractStdinCore {
-	return &AbstractStdinCore{}
+    return &AbstractStdinCore{}
 }
 
 var stdinCore = String{ len("stdin_core"), "stdin_core" }
 var coreStdinContext = &AbstractContext{
-	stdinCore,
-	coreStdinContextCreate,
-	coreStdinContextInit,
+    stdinCore,
+    coreStdinContextCreate,
+    coreStdinContextInit,
 }
 
 func coreStdinContextCreate(cycle *AbstractCycle) unsafe.Pointer {
-	/*
-	fmt.Printf("sssssssssssssssssssssssssssssssss: %s\n", cycle.Name)
-	if cycle.Name == stdin.Data.(string) {
-		fmt.Println("hahahahhahahah")
-
-	}
-	*/
-	return nil
+    return nil
 }
 
 func coreStdinContextInit(cycle *AbstractCycle, configure *unsafe.Pointer) string {
-	log := cycle.GetLog()
+    log := cycle.GetLog()
 
-	core := (*AbstractStdinCore)(unsafe.Pointer(configure))
-	if core.status == true {
-		core.AbstractFile = NewFile(log)
-	}
+    core := (*AbstractStdinCore)(unsafe.Pointer(configure))
+    if core.status == true {
+        core.AbstractFile = NewFile(log)
+    }
 
-	return "0"
+    return "0"
 }
 
 var (
-	status = String{ len("status"), "status" }
-	channal = String{ len("channal"), "channal" }
-	coreStdin AbstractStdinCore
+    status = String{ len("status"), "status" }
+    channal = String{ len("channal"), "channal" }
+    coreStdin AbstractStdinCore
 )
 
 var coreStdinCommands = []Command{
 
-	{ status,
-		MAIN_CONF|CONF_1MORE,
-		configureSetFlag,
-		0,
-		unsafe.Offsetof(coreStdin.status),
-		nil },
+    { status,
+      MAIN_CONF|CONF_1MORE,
+      configureSetFlag,
+      0,
+      unsafe.Offsetof(coreStdin.status),
+      nil },
 
-	{ channal,
-		MAIN_CONF|CONF_1MORE,
-		configureSetFlag,
-		0,
-		unsafe.Offsetof(coreStdin.channal),
-		nil },
+    { channal,
+      MAIN_CONF|CONF_1MORE,
+      configureSetFlag,
+      0,
+      unsafe.Offsetof(coreStdin.channal),
+      nil },
 
-	NilCommand,
+    NilCommand,
 }
 
 func configureSetFlag(configure *AbstractConfigure, command *Command, cycle *AbstractCycle) string {
-	value := configure.GetValue()
-	fmt.Println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-	fmt.Println(value)
-	return ""
+    value := configure.GetValue()
+    fmt.Println(value)
+    return ""
 }
 
 var coreStdinModule = Module{
-	MODULE_V1,
-	CONTEXT_V1,
-	unsafe.Pointer(coreStdinContext),
-	coreStdinCommands,
-	STDIN_MODULE,
-	coreStdinInit,
-	coreStdinMain,
+    MODULE_V1,
+    CONTEXT_V1,
+    unsafe.Pointer(coreStdinContext),
+    coreStdinCommands,
+    STDIN_MODULE,
+    coreStdinInit,
+    coreStdinMain,
 }
 
 func coreStdinInit(cycle *AbstractCycle) int {
-	return Ok
+    return Ok
 }
 
 func coreStdinMain(cycle *AbstractCycle) int {
-	/*
-		for ;; {
-			fmt.Println("aaaaaaaaaaa")
-		}
-		*/
-
-	return Ok
+    return Ok
 }
 
 func init() {
-	Modules = append(Modules, &coreStdinModule)
+    Modules = append(Modules, &coreStdinModule)
 }
