f69d27dddf74aac4f62480e4c7c726f5187531f4
mengshi <meng1985626@163.com>
2017-02-22 16:34:09 +0800

    update

--- modules/stdout_core_module.go
+++ modules/stdout_core_module.go
@@ -5,93 +5,87 @@
 package modules
 
 import (
-	"unsafe"
-	"fmt"
-	. "worker/types"
+      "unsafe"
+    . "worker/types"
 )
 
 type AbstractStdoutCore struct {
-	*AbstractCycle
-	*AbstractFile
+    *AbstractCycle
+    *AbstractFile
 
-	status   bool
-	channal  string
+     status   bool
+     channal  string
 }
 
 func NewStdoutCore() *AbstractStdoutCore {
-	return &AbstractStdoutCore{}
+    return &AbstractStdoutCore{}
 }
 
 var stdoutCore = String{ len("stdout_core"), "stdout_core" }
 var coreStdoutContext = &AbstractContext{
-	stdoutCore,
-	coreStdoutContextCreate,
-	coreStdoutContextInit,
+    stdoutCore,
+    coreStdoutContextCreate,
+    coreStdoutContextInit,
 }
 
 func coreStdoutContextCreate(cycle *AbstractCycle) unsafe.Pointer {
-	return nil
+    return nil
 }
 
 func coreStdoutContextInit(cycle *AbstractCycle, configure *unsafe.Pointer) string {
-	log := cycle.GetLog()
+    log := cycle.GetLog()
 
-	core := (*AbstractStdoutCore)(unsafe.Pointer(configure))
-	if core.status == true {
-		core.AbstractFile = NewFile(log)
-	}
+    core := (*AbstractStdoutCore)(unsafe.Pointer(configure))
+    if core.status == true {
+        core.AbstractFile = NewFile(log)
+    }
 
-	return "0"
+    return "0"
 }
 
 var (
-	coreStatus = String{ len("status"), "status" }
-	coreChannal = String{ len("channal"), "channal" }
-	coreStdout AbstractStdoutCore
+    coreStatus = String{ len("status"), "status" }
+    coreChannal = String{ len("channal"), "channal" }
+    coreStdout AbstractStdoutCore
 )
 
 var coreStdoutCommands = []Command{
 
-	{ coreStatus,
-		MAIN_CONF|CONF_1MORE,
-		configureSetFlag,
-		0,
-		unsafe.Offsetof(coreStdout.status),
-		nil },
+    { coreStatus,
+      MAIN_CONF|CONF_1MORE,
+      configureSetFlag,
+      0,
+      unsafe.Offsetof(coreStdout.status),
+      nil },
 
-	{ coreChannal,
-		MAIN_CONF|CONF_1MORE,
-		configureSetFlag,
-		0,
-		unsafe.Offsetof(coreStdout.channal),
-		nil },
+    { coreChannal,
+      MAIN_CONF|CONF_1MORE,
+      configureSetFlag,
+      0,
+      unsafe.Offsetof(coreStdout.channal),
+      nil },
 
-	NilCommand,
+    NilCommand,
 }
 
 var coreStdoutModule = Module{
-	MODULE_V1,
-	CONTEXT_V1,
-	unsafe.Pointer(coreStdoutContext),
-	coreStdoutCommands,
-	STDOUT_MODULE,
-	coreStdoutInit,
-	coreStdoutMain,
+    MODULE_V1,
+    CONTEXT_V1,
+    unsafe.Pointer(coreStdoutContext),
+    coreStdoutCommands,
+    STDOUT_MODULE,
+    coreStdoutInit,
+    coreStdoutMain,
 }
 
 func coreStdoutInit(cycle *AbstractCycle) int {
-	return Ok
+    return Ok
 }
 
 func coreStdoutMain(cycle *AbstractCycle) int {
-
-	for ;; {
-		fmt.Println("aaaaaaaaaaa")
-	}
-
-	return Ok
+    return Ok
 }
 
 func init() {
-	Modules = append(Modules, &coreStdoutModule)
+    Modules = append(Modules, &coreStdoutModule)
 }
